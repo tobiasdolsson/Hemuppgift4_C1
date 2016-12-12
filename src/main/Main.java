799c0e7c258e0f951d89b467d56d2b30630d518d
tobiasdolsson <tobias.d.olsson@hotmail.com>
2016-12-12 10:49:08 +0100

    lots of changes

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,7 +3,13 @@ package main;
 public class Main {
 	
 	public static void main(String args[]){
+		
 		TimingAttack ta = new TimingAttack("Kalle", "5");
-		System.out.println(ta.response());
+		for(int i=0; i<20; i++){
+			System.out.println("gissad symbol: "+ta.guessSymbol());
+		}
+		
+		//System.out.println(ta.responseTime("6"));
+		//ta.hexTest();
 	}
 }
--- src/main/TimingAttack.java
+++ src/main/TimingAttack.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
@@ -11,29 +12,76 @@ public class TimingAttack {
 	HttpURLConnection connection = null;
 	private String name, grade;
 	private String signature;
+	private long totalTime = 0;
 	private String targetURL = "https://eitn41.eit.lth.se:3119/ha4/addgrade.php?name=Kalle&grade=5&signature=";
+	private String startURL = "https://eitn41.eit.lth.se:3119/ha4/addgrade.php?name=Kalle&grade=5&signature=";
 	public TimingAttack(String name, String grade){
 		this.name = name;
 		this.grade=grade;
-		signature="6823000000000000ba36";
-		targetURL = targetURL + name + "&grade=" + grade + "&signature=" + signature;
+		//signature="6823ea50b133c58cba36"
+		//signature="6823000000000000ba36";
+		//signature="";
+		//targetURL = targetURL + name + "&grade=" + grade + "&signature=" + signature;
+	}
+	
+	public String getURL(){
+		return targetURL;
+	}
+	
+	public void setTargetURL(String URL){
+		targetURL = URL;
+	}
+	
+	public void setStartURL(String URL){
+		startURL = URL;
 	}
 	
 	public String getSignature(){
 		return signature;
 	}
 	
-	public String response(){
+	public String guessSymbol(){
+		BigInteger hex = new BigInteger("-1",16);
+		String guessedSymbol="";
+		float meanTime;
+		float currMeanTime=0;
+		for(int i=0; i<17; i++){
+			meanTime = responseTime(hex.toString(16));
+			setTargetURL(targetURL);
+			if(meanTime>currMeanTime && i!=0){
+				currMeanTime=meanTime;
+				guessedSymbol=hex.toString(16);
+				System.out.println(guessedSymbol);
+			}
+			hex=hex.add(BigInteger.ONE);
+		}
+		setTargetURL(targetURL+guessedSymbol);
+		setStartURL(targetURL+guessedSymbol);
+		return guessedSymbol;
+	}
+	
+	public long responseTime(String signature){
+		startURL = targetURL +signature;
+		System.out.println(startURL);
+		long startTime = System.nanoTime();
+		totalTime=0;
 		try {
+			
 		    //Create connection
-		    URL url = new URL(targetURL);
+			StringBuilder response = new StringBuilder(); // or StringBuffer if Java version 5+
+			for(int i = 0; i<5; i++){
+				
+			
+			URL url = new URL(startURL);
+			
 		    connection = (HttpURLConnection) url.openConnection();
-		 
+		    
 
 		    connection.setUseCaches(false);
 		    connection.setDoOutput(true);
 
 		    //Send request
+		    
 		    DataOutputStream wr = new DataOutputStream (
 		        connection.getOutputStream());
 		  //  wr.writeBytes(urlParameters);
@@ -42,18 +90,27 @@ public class TimingAttack {
 		    //Get Response  
 		    InputStream is = connection.getInputStream();
 		    BufferedReader rd = new BufferedReader(new InputStreamReader(is));
-		    StringBuilder response = new StringBuilder(); // or StringBuffer if Java version 5+
+		    
 		    String line;
 		    while ((line = rd.readLine()) != null) {
 		      response.append(line);
 		      response.append('\r');
 		    }
 		    rd.close();
-		    return response.toString();
+		    
+		    //System.out.println("Time: "+ (totalTime));
+		}
+			long endTime = System.nanoTime();
+		    totalTime = (endTime-startTime);
+			//System.out.println(startURL);
+		    System.out.println("Mean time " +signature+" :"+ (totalTime/5));
+		    return totalTime/5;
+			
 		  } catch (Exception e) {
 		    e.printStackTrace();
-		    return null;
+		    return 0;
 		  }
+		
 	}
 	
 }
